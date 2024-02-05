0044d4ea2e26cf4653045db80f0a1a0ff2f077fb
Jareste- <jareste2000@gmail.com>
2024-02-05 22:22:55 +0100

    seems more or less ok

--- srcs/ft_linear_regression.py
+++ srcs/ft_linear_regression.py
@@ -2,91 +2,65 @@ import csv
 import sys
 import os.path
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import numpy as np
 
-class linearRegression:
-    self.getData()
-    self.theta0 = 0
-    self.theta1 = 0
+class FtLinearRegression:
+    def __init__(self, learning_rate=0.01, iterations=10000):
+        self.theta0 = 0
+        self.theta1 = 0
+        self.learning_rate = learning_rate
+        self.iterations = iterations
 
-    def getData(self):
-        self.values = []
-        if os.path.isfile('data.csv') == False:
-            print("Error: File does not exist")
-            sys.exit(1)
-        if os.access('data.csv', os.R_OK) == False:
-            print("Error: File is not readable")
-            sys.exit(1)
-        with open('data.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                self.values.append(row)
+    def estimate_price(self, mileage):
+        return self.theta0 + self.theta1 * mileage
 
+    def fit(self, x, y):
+        m = len(x)
+        x_norm = (x - np.mean(x)) / (np.max(x) - np.min(x))  # normalize x values
+        y_norm = (y - np.mean(y)) / (np.max(y) - np.min(y))  # normalize y values
+        for i in range(self.iterations):
+            h = self.theta0 + self.theta1 * x_norm
+            loss = h - y_norm
+            gradient_theta0 = np.sum(loss) / m
+            gradient_theta1 = np.dot(x_norm, loss) / m
+            self.theta1 -= self.learning_rate * gradient_theta1
+            self.theta0 -= self.learning_rate * gradient_theta0
+            # if i % 100 == 0:
+            #     print(f'Iteration {i}/{self.iterations} - theta0: {self.theta0}, theta1: {self.theta1}')
+        self.theta0 = self.theta0 * (np.max(y) - np.min(y)) + np.mean(y) - self.theta1 * np.mean(x) * (np.max(y) - np.min(y)) / (np.max(x) - np.min(x))
+        self.theta1 = self.theta1 * (np.max(y) - np.min(y)) / (np.max(x) - np.min(x))
 
-def main():
-    if len(sys.argv) < 2:
-        print("Error: No file provided")
-        sys.exit(1)
-    
-    data = linearRegression()
-    data.
+    def predict(self, x):
+        return [self.estimate_price(xi) for xi in x]
 
-if __name__ == "__main__":
-    main()
+    def print(self):
+        print('theta0:', self.theta0)
+        print('theta1:', self.theta1)
 
-# #read csv file
-# data = []
+    def save(self, filename):
+        with open(filename, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.theta0, self.theta1])
 
-# with open('data.csv', 'r') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         data.append(row)
+sns.set(style='darkgrid')
 
-# # print(data)
+data = pd.read_csv('data.csv')
+data['km'] = pd.to_numeric(data['km'], errors='coerce')
+data['price'] = pd.to_numeric(data['price'], errors='coerce')
 
-# m = 0
-# b = 0
+x = data['km'].values
+y = data['price'].values
 
-# for i in range(1, len(data)):
-#     m += (int(data[i][0]) - int(data[1][0])) * (int(data[i][1]) - int(data[1][0]))
-#     b += (int(data[i][0]) - int(data[1][0])) ** 2
-#     print(m, b)
+model = FtLinearRegression()
+model.fit(x, y)
+data['predicted'] = model.predict(data['km'])
 
-# x = []
-# y = []
+sns.scatterplot(x='km', y='price', data=data)
+sns.lineplot(x='km', y='predicted', color='orange', lw=4, data=data)
 
-# for i in range(1, len(data)):
-#     x.append(int(data[i][0]))
-#     y.append(int(data[i][1]))
+model.print()
+model.save('model.csv')
 
-
-# # plt.plot(m, b, 'o')
-# plt.plot(x, y, 'o')
-# plt.show()
-# # y = m*x+ b
-
-
-
-
-#print graph
-# # importing the required module
-# import matplotlib.pyplot as plt
- 
-# # x axis values
-# x = [1,2,3]
-# # corresponding y axis values
-# y = [2,4,1]
- 
-# # plotting the points 
-# plt.plot(x, y)
- 
-# # naming the x axis
-# plt.xlabel('x - axis')
-# # naming the y axis
-# plt.ylabel('y - axis')
- 
-# # giving a title to my graph
-# plt.title('My first graph!')
- 
-# # function to show the plot
-# plt.show()
+plt.show()
--- srcs/predict.py
+++ srcs/predict.py
@@ -4,13 +4,10 @@ def get_parameters(file_name):
     try:
         with open(file_name, 'r') as file:
             reader = csv.reader(file)
-            next(reader)
             theta0, theta1 = next(reader)
-        try:
-            theta0, theta1 = next(reader)
-        except StopIteration:
-            print("The file is empty. Using default values for theta0 and theta1.")
-            theta0, theta1 = 0.0, 0.0
+    except StopIteration:
+        print("The file is empty. Using default values for theta0 and theta1.")
+        theta0, theta1 = 0.0, 0.0
     except FileNotFoundError:
         print("The file does not exist. Using default values for theta0 and theta1.")
         theta0, theta1 = 0.0, 0.0
@@ -20,7 +17,7 @@ def estimate_price(mileage, theta0, theta1):
     return theta0 + (theta1 * mileage)
 
 def main():
-    theta0, theta1 = get_parameters('model_parameters.csv')
+    theta0, theta1 = get_parameters('model.csv')
     mileage = float(input("Enter a mileage: "))
     price = estimate_price(mileage, theta0, theta1)
     print(f"The estimated price for a car with {mileage} mileage is {price}")
